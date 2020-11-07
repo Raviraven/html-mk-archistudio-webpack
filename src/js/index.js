c95f24286eb87561e49ccafbc25794ee5048b8cd
Michał Kruszewski <kruszewski2@gmail.com>
2020-11-07 19:32:00 +0100

    GMap fixed

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,11 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
+window.initMap = initMap;
+
 function initMap() {
     var uluru = {lat: 53.112874, lng: 23.146849};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 15, center: uluru});
+        document.getElementById('gmap'), {zoom: 15, center: uluru});
     var marker = new google.maps.Marker({position: uluru, map: map});
 }
-
-window.initMap = initMap;
