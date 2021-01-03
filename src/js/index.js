2c21a6425ea2b27f76f3ff5ef93cbc98598ef2a4
Michał Kruszewski <kruszewski2@gmail.com>
2021-01-03 19:13:43 +0100

    google maps - map removed

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
 import '../scss/main.scss';
 
 console.log('Welcome to the dark side 😈')
-
-window.initMap = initMap;
-
-function initMap() {
-    var uluru = {lat: 53.112874, lng: 23.146849};
-    var map = new google.maps.Map(
-        document.getElementById('gmap'), {zoom: 15, center: uluru});
-    var marker = new google.maps.Marker({position: uluru, map: map});
-}
