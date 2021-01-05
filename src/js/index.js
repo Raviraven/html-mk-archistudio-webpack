a1950cf0cdbe6002b48ba781133467be841efb30
Michał Kruszewski <kruszewski2@gmail.com>
2021-01-05 14:14:35 +0100

    spyscroll navigation - added

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
-import '../scss/main.scss';
-//import 'bootstrap';
-
 import {Modal, Carousel} from 'bootstrap';
+import '../scss/main.scss';
+
+import '../scss/bootstrap-custom.scss';
+
 
 console.log('Welcome to the dark side 😈')
