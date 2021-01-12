42e2ffefeaaf5aa3fbb72173c1f940936cea577c
Michał Kruszewski <kruszewski2@gmail.com>
2021-01-12 21:26:54 +0100

    Fixed arrow navigation when modal pops up

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
-import {Modal, Carousel} from 'bootstrap';
-import '../scss/main.scss';
+import { Modal, Carousel } from "bootstrap";
+import "../scss/main.scss";
 
-import '../scss/bootstrap-custom.scss';
+import "../scss/bootstrap-custom.scss";
 
+console.log("Welcome to the dark side 😈");
 
-console.log('Welcome to the dark side 😈')
+let modal = document.querySelector("#modal");
+let modalNextControl = document.querySelector("#carousel-control-next");
+
+modal.addEventListener("shown.bs.modal", function () {
+  modalNextControl.focus();
+});
